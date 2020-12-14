d16e4ac9319b67d97c3f05628cdec6a5e7af5b42
niccolosantelli <niccolo_santelli@live.it>
2020-12-14 16:08:52 +0100

    primo es vuejs

---
+++ main.js
@@ -0,0 +1,23 @@
+/*Istruzioni:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus: Aggiungere un’immagine presa anch’essa da un data*/
+
+let root = new Vue({
+  el:"#root",
+  data: {
+    changeText: "ciao ciao!!",
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI7We9blKl_iMQ73u13PmixpbFvUTSyukp8Q&usqp=CAU'
+  }
+});
+
+
+/*let root = new Vue({
+  el:"#root",
+  data: {
+
+  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI7We9blKl_iMQ73u13PmixpbFvUTSyukp8Q&usqp=CAU'
+},
+
+
+
+});*/
